aea5c96abd5a02f784e55220effbc7254224fd9f
Elias CROZE <elias.croze@gmail.com>
2014-11-02 13:34:58 +0800

    Detection of triangles crossing contrained edges

---
+++ basicsP2/trist.h
@@ -0,0 +1,154 @@
+#ifndef TRISTH
+#define TRISTH
+
+#include "pointSetArray.h"
+#include <vector>
+#include <set>
+#include "..\basics\li.h"
+
+/*
+
+  For a triangle abc, if version 0 is abc
+
+  version 0 abc     (v:012)
+  version 1 bca		(v:120)
+  version 2 cab		(v:201)
+  version 3 bac		(v:102)
+  version 4 cba		(v:210)
+  version 5 acb		(v:021)
+
+  enext cycles   0 > 1 > 2 > 0 ,  5 > 4 > 3 > 5
+  sym cycles  i <> (i + 3) % 6
+
+  org  = ver < 3 ? v[ver] : v[(ver+1)%3]
+  dest = ver < 3 ? v[(ver+1)%3] : v[ver-3] 
+
+*/
+
+
+
+typedef  int OrTri;  // The OrTri data structure for a triangle
+typedef  int FIndex; // The index of a triangle Hint: NOT a triangle if it's negative
+                     // You should be able to make all the triangle indices to be from 0 to n - 1 (n = number of triangles)
+
+class TriangulateState {
+public:
+	int step;
+	vector<int> linkPoints;
+	std::vector<int> bigTriangle;
+
+	bool isDone() { return step == -1; };
+
+	TriangulateState() { this->step = -1; };
+};
+
+class Trist;
+
+class TriRecord {
+protected:
+		int vi_[3];
+		OrTri fnext_[6];
+		int triIdx;
+		bool visible;
+	friend Trist;
+
+public:
+	bool getVisibility(){ return visible; }
+	int* getVertices(){ return vi_; }
+	int getIdx(){ return triIdx; }
+};
+
+class Edge{
+protected:
+	int vi_[2];
+	friend Trist;
+};
+
+class Trist {
+	private : 
+		PointSetArray pointSet;
+		vector<TriRecord> records;
+		int maxTriIdx;
+		TriRecord* findTriangle(int tIdx);
+		vector<int> bigTriangle;
+		vector<Edge> constrainedEdges;
+
+		set<int> pointsOnTri;
+		set<int> lastPointsAddedIdx;
+		set<int> lastTrianglesAddedIdx;
+		set<int> activeEdge;
+		int activePoint;
+		bool flipping;
+
+	protected:
+		int en_[6];
+
+	public:
+		Trist();
+		int addPoint(LongInt x, LongInt y);
+		int getPoint (int pIndex, LongInt& x1,LongInt& y1); // put the x,y values into x1,y1, and return 1 if the point pIndex exists
+		int noPt();                                         // return the number of points
+
+		int noTri(); // return the number of triangles
+		int makeTri(int pIndex1,int pIndex2,int pIndex3,bool autoMerge = false); // Add a triangle into the Trist with the three point indices
+		// Moreover, automatically establish the fnext pointers to its neigbhours if autoMerge = true
+
+		void delTri(OrTri ef); // Delete a triangle, but you can assume that this is ONLY used by the IP operation
+		                    // You may want to make sure all its neighbours are detached (below)
+		
+		void make3Tri(LongInt x, LongInt y);
+		vector<int> make3Tri(int pIdx); //the point already exists
+
+		OrTri enext(OrTri ef);
+		OrTri sym(OrTri ef);
+		OrTri fnext(OrTri ef);
+
+		void getVertexIdx(OrTri ef, int& pIdx1,int& pIdx2,int& pIdx3); // return the three indices of the three vertices by OrTri
+		int org(OrTri ef);  // the index of the first vertex of OrTri, e.g. org(bcd) => b
+		int dest(OrTri ef); // the index of the second vertex of OrTri, e.g. org(bcd) => c
+		void fmerge(OrTri abc, OrTri abd); // glue two neighbouring triangles, result abd = fnext(abc)
+		void fdetach(OrTri abc); // detach triangle abc with all its neighbours (undo fmerge)
+
+		//void incidentTriangles(int ptIndex,int& noOrTri, OrTri* otList); // A suggested function: you may want this function to return all the OrTri
+		                                                                 // that are incident to this point
+		                                                                 // Ignore this if you don't feel a need
+		OrTri inTriangle(int ptIndex); //if returns -1, we are not in any triangle (ptIndex is not the vertex of any triangle)
+		vector<int> adjacentTriangles(int pIdx1, int pIdx2); //indexes of triangles wich have pIdx1,pIdx2 as edge
+		vector<int> adjacentTriangles(int pIdx); //indexes of triangles wic<h have pIdx as vertex
+		
+		std::vector< std::pair<OrTri, int> > findNeighbours(TriRecord tri);
+
+		bool isLocallyDelaunay(int pIdx1, int pIdx2);
+		vector<int> flipEdge(int pIdx1, int pIdx2);
+		void flippingAlg(int pIdx1, int pIdx2);
+		void triangulate(); //we assume there is no triangle
+		TriangulateState *triangulateByPoint(int pIdx);
+		bool triangulateByPointStep(TriangulateState *state);
+		void hideBigTriangle(TriangulateState *state);
+		void showBigTriangles(TriangulateState *state);
+		void addPointUpdate(LongInt x, LongInt y);
+		std::vector<int> getTriIdx();
+		void setVisibility(int triIdx, bool visibility);
+		std::vector<TriRecord> getTriangles();
+		std::vector<MyPoint> getPoints();
+
+		void clearActive() { activeEdge.clear();
+						     lastPointsAddedIdx.clear();
+							 lastTrianglesAddedIdx.clear(); 
+							 activePoint = -1; };
+		void setActiveEdge(int pIdx1, int pIdx2) { activeEdge.clear(); activeEdge.insert(pIdx1); activeEdge.insert(pIdx2); };
+		void clearActiveEdge() { activeEdge.clear(); };
+		bool isPointOnTri(int pIdx) { return pointsOnTri.count(pIdx) == 1; };
+		bool isPointLastAdded(int pIdx) { return lastPointsAddedIdx.count(pIdx) == 1; };
+		bool isTriangleLastAdded(int tIdx) { return lastTrianglesAddedIdx.count(tIdx) == 1; };
+		bool isActiveEdge(int pIdx1, int pIdx2) { return activeEdge.count(pIdx1) == 1 && activeEdge.count(pIdx2) == 1; };
+		bool isActivePoint(int pIdx) { return activePoint == pIdx; };
+
+		int addConstrainedEdge(int pIdx1, int pIdx2);
+		void triangulateByPointCDT(int pIdx);
+		void triangulateByEdgeCDT(int edgeIdx);
+		void flippingAlgCDT(int pIdx1, int pIdx2);
+		bool constrained(int pIdx1, int pIdx2);
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,521 @@
+
+#include "basicsP2\pointSetArray.h"
+#include "basicsP2\trist.h"
+
+#include "math.h"
+#include <iostream>
+#include <fstream>
+#include <gl\glut.h>
+#include <windows.h>
+#include <cstdio>
+#include <fstream>
+#include <strstream>
+#include <string>
+#include <sstream>
+#include <ctime>
+#include <queue>
+#include "basics\stopWatch.h"
+
+using namespace std;
+
+double offset_x = 200;
+double offset_y = 200;
+int minX, minY, maxX, maxY;
+float k = 1;
+float tx = 0.0, ty=0.0;
+
+struct command {
+	string name;
+	LongInt arg1;
+	LongInt arg2;
+	command() {
+		name = "";
+		arg1 = LongInt::LongInt(0);
+		arg2 = LongInt::LongInt(0);
+	}
+};
+
+vector<int> addedPoints;
+vector<int> addedEdges;
+
+queue<command> cmdbuffer;
+Trist triangles;
+vector<int> pointBuffer;
+int dy_secs = 0;
+int dy_current = 0;
+bool jumpnext = false;
+
+bool intriangulate = false;
+int atPoint = 0;
+TriangulateState *triState;
+
+bool DEBUG = true;
+
+int startTime1, startTime2, endTime1, endTime2;
+SYSTEMTIME st_1;
+SYSTEMTIME st_2;
+
+void drawAPoint(double x, double y, bool isNew = false)
+{
+	glPointSize(5);
+	glBegin(GL_POINTS);
+	if (isNew)
+		glColor3f(0, 1, 0);
+	else
+		glColor3f(0.5, 0.5, 0.5);
+	glVertex2d(x, y);
+	glEnd();
+	glPointSize(1);
+}
+
+void drawAPoint(double x, double y, float r, float g, float b)
+{
+	glPointSize(5);
+	glBegin(GL_POINTS);
+	glColor3f(r, g, b);
+	glVertex2d(x, y);
+	glEnd();
+	glPointSize(1);
+}
+
+void drawALine(double x1, double y1, double x2, double y2, bool isNew = false)
+{
+	glPointSize(1);
+	glBegin(GL_LINE_LOOP);
+	if (isNew)
+		glColor3f(0.7, 0.7, 0);
+	else
+		glColor3f(0, 0, 1);
+	glVertex2d(x1, y1);
+	glVertex2d(x2, y2);
+	glEnd();
+	glPointSize(1);
+}
+
+void drawATriangle(double x1, double y1, double x2, double y2, double x3, double y3, bool isNew = false)
+{
+	glBegin(GL_POLYGON);
+	if (isNew)
+		glColor3f(0.75, 0, 0);
+	else
+		glColor3f(0, 0.5, 0);
+	glVertex2d(x1, y1);
+	glVertex2d(x2, y2);
+	glVertex2d(x3, y3);
+	glEnd();
+}
+
+int currenttime()
+{
+	SYSTEMTIME st;
+	GetLocalTime(&st);
+	int current = (((st.wHour * 60 + st.wMinute) * 60) + st.wSecond) * 1000 + st.wMilliseconds;
+	return current;
+}
+
+void runcmd(command cmd)
+{
+	if (DEBUG)
+		cout << cmd.name << " " << cmd.arg1.printOut() << " " << cmd.arg2.printOut() << endl;
+
+	if (!cmd.name.compare("CD")){
+		if (pointBuffer.size() > 0){
+			// TIMER COUNTER INITIALISE FOR THIS "CD" STEP
+			GetLocalTime(&st_1);
+			int startTime1 = (((st_1.wHour*60+st_1.wMinute)*60)+st_1.wSecond)*1000+st_1.wMilliseconds;
+			intriangulate = true;
+			atPoint = 1;
+			triState = triangles.triangulateByPoint(pointBuffer.at(0));
+		}
+	}
+	else if (!cmd.name.compare("IP")) {
+		if (cmd.arg1 >= minX && cmd.arg1 <= maxX && cmd.arg2 >= minY && cmd.arg2 <= maxY){
+			pointBuffer.push_back(triangles.addPoint(cmd.arg1, cmd.arg2));
+		}
+		else if (DEBUG) {
+			cout << "Point does not fit in window : " << cmd.arg1.printOut() << "," << cmd.arg2.printOut() << std::endl;
+		}
+	}
+	cerr << "Overall Length in millisecond: " << endTime2-startTime2+endTime1-startTime1 << endl;
+}
+
+void updatescene(void)
+{
+	int cur = currenttime();
+	if (dy_secs == 0 || jumpnext || ((cur - dy_current) >= dy_secs * 1000))
+	{
+		jumpnext = false;
+		if (intriangulate){
+			// TIMER START FOR THIS STEP
+			GetLocalTime(&st_2);
+			int startTime2 = (((st_2.wHour*60+st_2.wMinute)*60)+st_2.wSecond)*1000+st_2.wMilliseconds;
+			if (!triState->isDone()){
+				triangles.triangulateByPointStep(triState);
+			}
+			else{
+				if (atPoint < pointBuffer.size()){
+					delete triState;
+					triState = triangles.triangulateByPoint(pointBuffer.at(atPoint));
+				}
+				else{
+					triangles.hideBigTriangle(triState);
+					triangles.clearActive();
+					delete triState;
+					intriangulate = false;
+					pointBuffer.clear();
+					// "CD" STEP DONE! ADD THIS TIMER TO GET FINAL TIME FOR TRIANGULATION
+					GetLocalTime(&st_2);
+					int endTime1 = (((st_1.wHour*60+st_1.wMinute)*60)+st_1.wSecond)*1000+st_1.wMilliseconds;
+				}
+				atPoint++;
+			}
+			// TIMER STOP FOR THIS STEP, ADD TO TIMER COUNTER
+			GetLocalTime(&st_2);
+			int endTime2 = (((st_2.wHour*60+st_2.wMinute)*60)+st_2.wSecond)*1000+st_2.wMilliseconds;
+			//cerr << "Overall Length in millisecond(including file output): " << endTime2-startTime2+endTime1-startTime1 << endl;
+			glutPostRedisplay();
+		}
+		else if (cmdbuffer.size() > 0){
+			command cmd = cmdbuffer.front();
+			cmdbuffer.pop();
+			runcmd(cmd);
+			string last_cmd = cmd.name;
+
+			// run multiple similiar commands together
+			while (cmdbuffer.size() != 0){
+				cmd = cmdbuffer.front();
+				if (!cmd.name.compare(last_cmd)){
+					cmdbuffer.pop();
+					runcmd(cmd);
+				}
+				else
+					break;
+			}
+			glutPostRedisplay();
+		}
+		dy_current = cur;
+	}
+}
+
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glPushMatrix();
+
+	//controls transformation
+	glScalef(k, k, k);	
+	glTranslatef(tx, ty, 0);	
+
+	int nbPoint = triangles.noPt();
+	int p1Idx, p2Idx, p3Idx;
+	LongInt px1, py1, px2, py2, px3, py3;
+	bool isNew = true;
+
+	glTranslated(offset_x,offset_y,0);
+	vector<TriRecord> record = triangles.getTriangles();
+	for(vector<TriRecord>::iterator it = record.begin(); it != record.end(); ++it){
+		if(it->getVisibility()){
+			int *vert = it->getVertices();
+			p1Idx = vert[0];
+			p2Idx = vert[1];
+			p3Idx = vert[2];
+			triangles.getPoint(p1Idx, px1, py1);
+			triangles.getPoint(p2Idx, px2, py2);
+			triangles.getPoint(p3Idx, px3, py3);
+
+			isNew = triangles.isTriangleLastAdded(it->getIdx());
+			drawATriangle(px1.doubleValue(), py1.doubleValue(),
+						  px2.doubleValue(), py2.doubleValue(),
+						  px3.doubleValue(), py3.doubleValue(), isNew);
+			drawALine(px1.doubleValue(), py1.doubleValue(),
+					  px2.doubleValue(), py2.doubleValue(), triangles.isActiveEdge(p1Idx, p2Idx));
+			drawALine(px2.doubleValue(), py2.doubleValue(),
+					  px3.doubleValue(), py3.doubleValue(), triangles.isActiveEdge(p2Idx, p3Idx));
+			drawALine(px3.doubleValue(), py3.doubleValue(),
+					  px1.doubleValue(), py1.doubleValue(), triangles.isActiveEdge(p3Idx, p1Idx));
+		}
+	}
+
+	vector<MyPoint> points = triangles.getPoints();
+	int i = 0;
+	for (vector<MyPoint>::iterator it = points.begin(); it != points.end(); ++it) {
+		if(it->visible){
+			isNew = triangles.isActivePoint(i) || triangles.isPointLastAdded(i);
+			if (isNew)
+				drawAPoint(it->x.doubleValue(), it->y.doubleValue(), isNew);
+			else if (triangles.isPointOnTri(i))
+				drawAPoint(it->x.doubleValue(), it->y.doubleValue(), 0, 0, 0);
+			else
+				drawAPoint(it->x.doubleValue(), it->y.doubleValue());
+		}
+		i++;
+	}
+
+	glPopMatrix();
+	glutSwapBuffers();
+}
+
+void reshape (int w, int h)
+{
+
+	glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+	glMatrixMode (GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0,w,h,0);  
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+
+}
+
+void init(void)
+{
+	glClearColor (1.0,1.0,1.0, 1.0);
+}
+
+void readFile(){
+
+	string line_noStr;
+	string line;   // each line of the file
+	string cmd;// the command of each line
+	string numberStr; // for single LongInt operation
+	string outputAns = "Answer of your computation"; // the answer you computed
+	ifstream inputFile("input.txt",ios::in);
+
+	if(inputFile.fail()){
+		cerr << "Error: Cannot read input file \"" << "input.txt" << "\"";
+		return;
+	}
+
+	time_t curtime = time(NULL);
+	while(inputFile.good()){
+		getline(inputFile,line);
+		if(line.empty()) {
+			cmd = "";
+			continue; 
+		}// in case the line has nothing in it
+
+		stringstream linestream(line);
+
+		linestream >> line_noStr;
+		linestream >> cmd;         // get the command
+
+		if(!cmd.compare("IP")){
+			linestream >> numberStr;
+			LongInt p1 = LongInt::LongInt(numberStr.c_str());
+			linestream >> numberStr;
+			LongInt p2 = LongInt::LongInt(numberStr.c_str());
+			/*command newcmd;
+			newcmd.name = "IP";
+			newcmd.arg1 = p1;
+			newcmd.arg2 = p2;
+			cmdbuffer.push(newcmd);*/
+			addedPoints.push_back(triangles.addPoint(p1, p2));
+		}
+		else if(!cmd.compare("DY")){
+			linestream >> numberStr;
+			dy_secs = atof(numberStr.c_str());
+			if(dy_secs < 0)
+				dy_secs = 0;
+		}
+		else if(!cmd.compare("CD")){
+			command newcmd;
+			newcmd.name = "CD";
+			cmdbuffer.push(newcmd);
+			//cerr << "Overall Length in millisecond: " << endTime2-startTime2+endTime1-startTime1 << endl;
+		}
+		else if (!cmd.compare("CE")){
+			linestream >> numberStr;
+			int pIdx1 = atof(numberStr.c_str()) - 1;
+			linestream >> numberStr;
+			int pIdx2 = atof(numberStr.c_str()) - 1;
+			addedEdges.push_back(triangles.addConstrainedEdge(pIdx1, pIdx2));
+		}
+		else if (!cmd.compare("CDT")){
+			vector<int>::iterator it;
+			for (it = addedPoints.begin(); it != addedPoints.end(); it++){
+				triangles.triangulateByPointCDT(*it);
+			}
+			for (it = addedEdges.begin(); it != addedEdges.end(); it++){
+				triangles.triangulateByEdgeCDT(*it);
+			}
+			
+			addedPoints.clear();
+			addedEdges.clear();
+		}
+		else{
+			cerr << "Exception: Wrong input command" << endl;
+		}
+		//cerr << "Overall Length in millisecond(including file output): " << endTime2-startTime2+endTime1-startTime1 << endl;
+		
+	}
+}
+
+void writeFile()
+{
+	ofstream outputFile("savefile.txt",ios::out, ios_base::trunc);
+	int no_line = 1;
+	int p1Idx, p2Idx, p3Idx;
+	OrTri tri;
+	LongInt px, py;
+	vector<MyPoint> points = triangles.getPoints();
+	for (vector<MyPoint>::iterator it = points.begin(); it != points.end(); ++it) {
+		if(it->visible){
+			outputFile << no_line << ": IP " << it->x.printOut().c_str() << " " << it->y.printOut().c_str() << endl;
+			no_line++;
+		}
+	}
+	outputFile << no_line << ": CD " << endl;
+}
+
+void keyboard (unsigned char key, int x, int y)
+{
+	switch (key) {
+		case 'i':
+		case 'I':
+			k += 0.1;
+			glutPostRedisplay();
+		break;
+
+		case 'c':
+		case 'C':
+			glutPostRedisplay();
+		break;
+
+		case 'o':
+		case 'O':
+			if(k>0.1)
+				k-=0.1;
+			glutPostRedisplay();
+		break;
+			
+		case 'r':
+		case 'R':
+			readFile();
+		break;
+
+		case 'w':
+		case 'W':
+			writeFile();
+		break;
+
+		case 'Q':
+		case 'q':
+			exit(0);
+		break;
+
+		case 's':
+		case 'S':
+			writeFile();
+			exit(0);
+		break;
+
+		case 'L':
+		case 'l':
+			tx-= 1;
+			glutPostRedisplay();
+		break;
+
+		case 'a':
+		case 'A':
+			tx+= 1;
+			glutPostRedisplay();
+		break;
+
+		case 'd':
+		case 'D':
+			ty-= 1;
+			glutPostRedisplay();
+		break;
+
+		case 'u':
+		case 'U':
+			ty+= 1;
+			glutPostRedisplay();
+		break;
+
+		default:
+		break;
+	}
+}
+
+void specialkeys(int key, int x, int y)
+{
+	switch (key) {
+	case GLUT_KEY_RIGHT:
+		if (dy_secs > 0 && !jumpnext) {
+			jumpnext = true;
+			glutPostRedisplay();
+		}
+		break;
+
+	default:
+		break;
+	}
+}
+
+void mouse(int button, int state, int x, int y)
+{
+	/*button: GLUT_LEFT_BUTTON, GLUT_MIDDLE_BUTTON, or GLUT_RIGHT_BUTTON */
+	/*state: GLUT_UP or GLUT_DOWN */
+	enum
+	{
+		MOUSE_LEFT_BUTTON = 0,
+		MOUSE_MIDDLE_BUTTON = 1,
+		MOUSE_RIGHT_BUTTON = 2,
+		MOUSE_SCROLL_UP = 3,
+		MOUSE_SCROLL_DOWN = 4
+	};
+	if((button == MOUSE_RIGHT_BUTTON)&&(state == GLUT_UP))
+	{
+		x = x + minX;
+		y = y + minY; 
+		if (DEBUG) {
+			std::cout << "clicked" << std::endl;
+			std::cout << x << ", " << y << std::endl;
+		}
+		if(x >= minX && x <= maxX && y >= minY && y <= maxY){ 
+			triangles.addPointUpdate(LongInt::LongInt(x), LongInt::LongInt(y));
+		}
+		cerr << "Overall Length in millisecond: " << endTime2-startTime2+endTime1-startTime1 << endl;
+	}
+
+	glutPostRedisplay();
+}
+
+int main(int argc, char **argv)
+{
+	cout<<"CS5237 phase ii"<< endl<< endl;
+	cout << "right mouse click: ot operation"<<endl;
+	cout <<"i/o: zoom in/out"<<endl;
+	cout <<"l/a: move left/right"<<endl;
+	cout <<"u/d: move up or down"<<endl;
+	cout << "Right arrow to jump to next step" << endl;
+	cout << "q: quit" <<endl;
+	cout << "r: read in control points from \"input.txt\"" <<endl;
+	cout << "w: write control points to \"savefile.txt\"" <<endl;
+	cout << "s: saving the final triangulation to \"savefile.txt\" and quit the program"<<endl;
+	glutInit(&argc, argv);
+	glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize (1000, 700);
+
+	minX = -(GLUT_WINDOW_WIDTH + offset_x/2);
+	minY = -(GLUT_WINDOW_HEIGHT + offset_y/2);
+	maxX = 1000 + minX;
+	maxY = 700 + minY;
+	
+
+	glutInitWindowPosition(50, 50);
+	glutCreateWindow ("CS5237 Phase II");
+	init ();
+	glutDisplayFunc(display);
+	glutIdleFunc(updatescene);
+	glutReshapeFunc(reshape);
+	glutMouseFunc(mouse);
+	glutKeyboardFunc(keyboard);
+	glutSpecialUpFunc(specialkeys);
+	glutMainLoop();
+
+	return 0;
+}
